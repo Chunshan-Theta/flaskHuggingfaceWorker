3b45bf660a5165421c179665272007ea7c175546
gavin mac pro <gavin19950511@gmail.com>
2022-06-04 16:15:14 +0800

    refactor: objectize pipeline interface

--- predict.py
+++ predict.py
@@ -1,11 +1,35 @@
-from transformers import AutoModelForSequenceClassification,pipeline,AutoModelForSeq2SeqLM
+from transformers import AutoModelForSequenceClassification, pipeline, AutoModelForSeq2SeqLM
 from config import MODEL_NAME, MODEL_VERSION, MODEL_TOKENIZER, TASK_TYPE
 from transformers import AutoTokenizer
-import numpy as np
 
 
+class Pipeline:
+    def __init__(self, task, model, tokenizer):
+        self.task = task
+        self.model = model
+        self.tokenizer = tokenizer
+        self._pipeline = None
+
+    def __call__(self,text):
+        return self._pipeline(text)
 
 
+class TextClassifyPipeline(Pipeline):
+    def __init__(self, model_name, model_tokenizer, model_version):
+        model = AutoModelForSequenceClassification.from_pretrained(model_name, revision=model_version)
+        tokenizer = AutoTokenizer.from_pretrained(model_tokenizer)
+        task = "text-classification"
+        super().__init__(task, model, tokenizer)
+        self._pipeline = pipeline(self.task, model=self.model, tokenizer=self.tokenizer, return_all_scores=True)
+
+
+class Text2TextPipeline(Pipeline):
+    def __init__(self, model_name, model_tokenizer, model_version):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name, revision=model_version)
+        tokenizer = AutoTokenizer.from_pretrained(model_tokenizer)
+        task = "text2text-generation"
+        super().__init__(task, model, tokenizer)
+        self._pipeline = pipeline(self.task, model=self.model, tokenizer=self.tokenizer)
 
 
 
@@ -13,17 +37,13 @@ def getPipeline(TASK_TYPE):
     _pipeline = None
 
     if "text-classification" == TASK_TYPE:
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, revision=MODEL_VERSION)
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_TOKENIZER)
-        _pipeline = pipeline("text-classification", model=model, tokenizer=tokenizer,return_all_scores=True)
+        _pipeline = TextClassifyPipeline(MODEL_NAME, MODEL_TOKENIZER, MODEL_VERSION)
 
     if "text2text-generation" == TASK_TYPE:
-        model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME, revision=MODEL_VERSION)
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_TOKENIZER)
-        _pipeline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
-
+        _pipeline = Text2TextPipeline(MODEL_NAME, MODEL_TOKENIZER, MODEL_VERSION)
 
     return _pipeline
+
 
 PipelineInterface = getPipeline(TASK_TYPE)
 PipelineInterface("你好嗎")
